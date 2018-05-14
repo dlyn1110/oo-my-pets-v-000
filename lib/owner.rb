af782bbffcc46753ad5d4ad683c561d64e788ae4
Dani Lyn <dlyn1110@gmail.com>
2018-05-14 16:33:15 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,6 +61,6 @@ def sell_pets
 end
 
 def list_pets
-  owner.list_pets = "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s).")
+ "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s).")
 end
 end
